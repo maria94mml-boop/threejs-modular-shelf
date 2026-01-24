d4f7c5a69b8d6d5fb105ef72e9e1da36dac94318
“María <maria94mml@gmail.com>
2026-01-24 17:23:08 +0100

    Initial commit - Basic structure with three.js

---
+++ main.js
@@ -0,0 +1,6 @@
+import MaterialManager from './js/MaterialManager.js';
+import UIManager from './js/UIManager.js';
+import ProductLogic from './js/ProductLogic.js';
+
+let scene, camera, renderer, chair, controls;
+let materialManager, productLogic, uiManager;
