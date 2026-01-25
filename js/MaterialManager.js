db4915bd36f0d776998d3a0aae7c301ea3646d23
“María <maria94mml@gmail.com>
2026-01-25 11:51:34 +0100

    Add material manager and apply base wood texture

--- js/MaterialManager.js
+++ js/MaterialManager.js
@@ -1,2 +1,45 @@
 export default class MaterialManager {
+    constructor() {
+        this.loader = new THREE.TextureLoader();
+        this.materials = {};
+    }
+
+    /** Carga una madera base*/ 
+    loadWood049() {
+        if (this.materials.wood049) {
+            return this.materials.wood049;
+        }
+
+        const color = this.loader.load("./textures/structure/wood049/color.jpg");
+        const normal = this.loader.load("./textures/structure/wood049/normal.jpg");
+        const roughness = this.loader.load("./textures/structure/wood049/rough.jpg");
+
+        [color, normal, roughness].forEach(tex => {
+            tex.wrapS = THREE.RepeatWrapping;
+            tex.wrapT = THREE.RepeatWrapping;
+            tex.repeat.set(1, 1);
+        });
+
+        const material = new THREE.MeshStandardMaterial({
+            map: color,
+            normalMap: normal,
+            roughnessMap: roughness,
+            roughness: 1,
+            metalness: 0
+        });
+
+        this.materials.wood049 = material;
+        return material;
+    }
+
+    getMaterial(type) {
+        switch (type) {
+        case "wood049":
+            return this.loadWood049();
+        default:
+            console.warn("Material no definido:", type);
+            return null;
+        }
+    }
+    
 }
--- js/ProductLogic.js
+++ js/ProductLogic.js
@@ -1,9 +1,12 @@
+import MaterialManager from "./MaterialManager.js";
 export default class ProductLogic {
     constructor(baseModule, parentGroup) {
         this.baseModule = baseModule;
         this.group = parentGroup;
         this.levels = 1;
         this.modules =[];
+        this.MaterialManager = new MaterialManager();
+        this.currentMaterial = this.MaterialManager.getMaterial("wood049");
         this.buildShelf();
     }
 
@@ -18,10 +21,17 @@ export default class ProductLogic {
     buildShelf() {
         this.modules.forEach(m => this.group.remove(m));
         this.modules = [];
+        
+
 
         for (let i = 0; i < this.levels; i++) {
             const module = this.baseModule.clone(true);
             module.position.y = i * 30; // altura del módulo en cm
+            module.traverse(child => {
+                if (child.isMesh) {
+                    child.material = this.currentMaterial;
+                }
+            });
             this.group.add(module);
             this.modules.push(module);
         }
@@ -31,7 +41,7 @@ export default class ProductLogic {
     addLevel() {
         this.setLevels(this.levels + 1);
     }
-    
+
     removeLevel() {
         this.setLevels(this.levels - 1);
     }   
